fc28c55117c2df29b54eceadfc9dbafbbeb17cd0
Danilo Suguimoto <danilo.suguimoto@webjump.com.br>
2021-05-04 17:30:45 -0300

    Adding phpdoc to database DBConnection class

--- app/DatabaseConnection/DBConnection.php
+++ app/DatabaseConnection/DBConnection.php
@@ -6,16 +6,28 @@ namespace WjCrypto\DatabaseConnection;
 
 use PDO;
 
+/**
+ * DBConnection
+ */
 class DBConnection {
 
-  protected static $connection;
-
-  public function __construct()
-  {
+  private static $connection;
+  
+  /**
+   * __construct
+   *
+   * @return void
+   */
+  public function __construct() {
     $connection = new PDO("mysql:host=localhost; port=3306; dbname=wj_crypto;", "root", "t8n58jpOy-1Bg2PCkP17gflhGie1oRCV");
     self::$connection = $connection;
   }
-
+  
+  /**
+   * getDBConnection
+   *
+   * @return PDO
+   */
   public function getDBConnection() {
     return self::$connection;
   }
