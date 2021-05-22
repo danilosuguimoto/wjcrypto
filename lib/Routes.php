267507fa122577139950c5bba2e768799b9fba86
Danilo Suguimoto <danilo.suguimoto@webjump.com.br>
2021-05-22 15:07:03 -0300

    Adding a prefix to all API routes

--- lib/Routes.php
+++ lib/Routes.php
@@ -8,12 +8,19 @@ use WjCrypto\Controllers\Frontend\UserController;
 
 //API Controllers
 SimpleRouter::group(['namespace' => 'WjCrypto\Controllers\API'], function() {
-  SimpleRouter::get('/users', [UserApiController::class, 'getAllUsers']);
-  SimpleRouter::get('/users/{id}', [UserApiController::class, 'getUserById']);
-  SimpleRouter::get('/users/create', [UserApiController::class, 'createUser']);
+  SimpleRouter::group(['prefix' => '/rest/API'], function() {
+    //User routes
+    SimpleRouter::group(['prefix' => '/users'], function() {
+      SimpleRouter::get('', [UserApiController::class, 'getAllUsers']);
+      SimpleRouter::get('/search/{id}', [UserApiController::class, 'getUserById']);
+      
+      SimpleRouter::post('/create', [UserApiController::class, 'createUser']);
+    });
+  });
 });
 
 //Frontend Controllers
 SimpleRouter::group(['namespace' => 'WjCrypto\Controllers\Frontend'], function() {
   SimpleRouter::get('/', [UserController::class, 'showHomePage']);
+  SimpleRouter::post('/login', [UserController::class, 'login']);
 });
