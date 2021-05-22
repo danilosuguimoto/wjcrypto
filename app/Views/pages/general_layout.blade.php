8ce7eebd77cae9ac283e0bb4802492505ef0883a
Danilo Suguimoto <danilo.suguimoto@webjump.com.br>
2021-05-22 15:14:33 -0300

    Changing the structure for the general layout

--- app/Views/pages/general_layout.blade.php
+++ app/Views/pages/general_layout.blade.php
@@ -1,16 +1,35 @@
-<!-- Stored in Views/general_layout.blade.php -->
-
+<!-- Stored in Views/pages/general_layout.blade.php -->
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>WjCrypto - @yield('title')</title>
+		<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="icon" type="image/png" href="Misc/Icon/wj.jpeg">
+		<link rel="stylesheet" href="Misc/CSS/mainstyle.css">
+		@yield('head')
 	</head>
 	<body>
-		@section('sidebar')
-			This is the master sidebar.
-		@show
-
-		<div class="container">
-			@yield('content')
+		<header>
+			<h1 id="title">
+				WJ<span id="title-color2">Crypto</span>
+			</h1>
+			@yield('header')
+		</header>
+		<div class="content-wrapper">
+			<div id="bg-opacity"></div>
+			<nav></nav>
+			<main>
+				@yield('content')
+			</main>
 		</div>
+		@section('footer')
+			<footer>
+				<div id="developer-name">
+					<span>
+						Desenvolvido por <strong>{{ $name }}</strong>
+					</span>
+				</div>
+			</footer>
+		@show
 	</body>
 </html>
