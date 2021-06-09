d4836d983e7f324e959d2ec706e6d1c7259d7844
Danilo Suguimoto <danilo.suguimoto@webjump.com.br>
2021-06-09 15:56:48 -0300

    Changing the structure for the login and home blade pages

--- app/Views/pages/home.blade.php
+++ app/Views/pages/home.blade.php
@@ -8,11 +8,45 @@
 @endsection
 
 @section('header')
-  <div class="welcome-message shadow">
-    <span id='test1'>Bem vindo(a), {{ $username }}!</span>
+  <div class="header-button shadow">
+    <a href="/logout">Logout</a>
+  </div>
+@endsection
+
+@section('page-title')
+  <h2>Home</h2>
+  <div class="header-balance">
+    <h3>Saldo Atual: 
+      @if ($balance < 1000)
+        <span id="balance-bad">R$ {{$balance}}</span>
+      @else
+        <span id="balance-good">R$ {{$balance}}</span>
+      @endif
+    </h3>
   </div>
 @endsection
 
 @section('content')
-  
+  <div class="menu">
+    <section>
+      <h1 class="greetings">Bem vindo(a) à <span id="welcome-color1">WJ</span><span id="welcome-color2">Crypto</span>!</h1>
+      <hr class="menu-hr">
+      <h2>O que você deseja fazer?</h2>
+    </section>
+
+    <div id="options">
+      <div id="m1" class="menu-link shadow">
+        <a href="/deposit">Depositar</a>
+      </div>
+      <div id="m2" class="menu-link shadow">
+        <a href="/withdraw">Sacar</a>
+      </div>
+      <div id="m3" class="menu-link shadow">
+        <a href="/transfer">Transferir</a>
+      </div>
+      <div id="m4" class="menu-link shadow">
+        <a href="/balance">Ver Extrato</a>
+      </div>
+    </div>
+  </div>
 @endsection
--- app/Views/pages/login.blade.php
+++ app/Views/pages/login.blade.php
@@ -3,24 +3,48 @@
 @extends('general_layout')
 
 @section('head')
-  <title>Home Page</title>
+  <title>Login</title>
   <link rel="stylesheet" href="Misc/CSS/pages/login.css">
 @endsection
 
 @section('header')
-  <p id="create-account">
-    <a href="#">Criar Conta</a>
+  <p class="header-button shadow">
+    <a href="/createAccount">Criar Conta</a>
   </p>
 @endsection
 
+@section('page-title')
+  <h2>Login</h2>
+@endsection
+
+@if (isset($message))
+  @section('alert')
+  <div class="alert shadow">
+    <div>{{ $message }}</div>
+  </div>
+  @endsection
+@endif
+
 @section('content')
-  <div class="forms login-form">
-    <form action="login" method="post">
-      <label class="login-labels shadow" for="acc_number">Número da conta: </label>
-      <input class="login-inputs shadow" type="text" name="acc_number" id="acc_number">
-      <label class="login-labels shadow" for="password">Senha: </label>
-      <input class="login-inputs shadow" type="password" name="password" id="password">
-      <input id="login" class="shadow" type="submit" value="Entrar">
+  <div class="login-form box-gradient-bg">
+    <form class="forms" action="loginPost" method="post">
+      <section>
+        <h3 class="form-title">Informações de usuário</h3>  
+      </section>
+
+      <hr class="form-hr">
+
+      <label class="labels shadow" for="acc_number">Número da conta: </label>
+      <input class="inputs shadow" type="text" name="acc_number" id="acc_number" required>
+
+      <label class="labels shadow" for="password">Senha: </label>
+      <input class="inputs shadow" type="password" name="password" id="password" required>
+
+      <input id="login" class="submit-button shadow" type="submit" value="Entrar">
+      <div id="doesnt-have-an-account" class="shadow">
+        <hr class="form-hr">
+        Não tem uma conta? <a href="/createAccount">Crie uma aqui</a>
+      </div>
     </form>
   </div>
 @endsection
