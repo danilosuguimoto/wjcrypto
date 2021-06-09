c111525117e32f3167f62e8d4559416d4f89e67b
Danilo Suguimoto <danilo.suguimoto@webjump.com.br>
2021-06-09 16:02:17 -0300

    Adding new constants and attributes to TransactionModel

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -9,20 +9,31 @@ use WjCrypto\Models\CoreModel;
 /**
  * TransactionModel
  */
-class TransactionModel extends CoreModel {
+class TransactionModel extends CoreModel 
+{
+  public const TRANSACTION_DEPOSIT_SUCCESS = 'Depósito realizado com sucesso!';
+  public const TRANSACTION_WITHDRAW_SUCCESS = 'Saque realizado com sucesso!';
+  public const TRANSACTION_TRANSFER_SUCCESS = 'Transferência realizada com sucesso!';
+  public const TRANSACTION_INSUFFICIENT_BALANCE = 'Você não possui saldo suficiente para esta transação.';
+  public const TRANSACTION_ACC_NUMBER_NOT_FOUND = 'O número da conta informada não existe.';
+
   private $transaction_id;
   private $acc_number;
+  private $amount;
   private $type;
   private $created_at;
   private $from_acc;
   private $to_acc;
 
-  public function __construct() {
+  public function __construct()
+  {
     parent::setAttributes(
       "wj_crypto.transactions",
       [
         "transaction_id",
         "acc_number",
+        "amount",
+        "balance",
         "type",
         "created_at",
         "from_acc",
@@ -34,7 +45,8 @@ class TransactionModel extends CoreModel {
   /**
    * Get the value of transaction_id
    */ 
-  public function getTransactionId() {
+  public function getTransactionId()
+  {
     return $this->transaction_id;
   }
 
@@ -43,7 +55,8 @@ class TransactionModel extends CoreModel {
    *
    * @return  self
    */ 
-  public function setTransactionId($transaction_id) {
+  public function setTransactionId($transaction_id)
+  {
     $this->transaction_id = $transaction_id;
 
     return $this;
@@ -52,7 +65,8 @@ class TransactionModel extends CoreModel {
   /**
    * Get the value of acc_number
    */ 
-  public function getAccNumber() {
+  public function getAccNumber()
+  {
     return $this->acc_number;
   }
 
@@ -61,8 +75,49 @@ class TransactionModel extends CoreModel {
    *
    * @return  self
    */ 
-  public function setAccNumber($acc_number) {
+  public function setAccNumber($acc_number)
+  {
     $this->acc_number = $acc_number;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of amount
+   */ 
+  public function getAmount()
+  {
+    return $this->amount;
+  }
+
+  /**
+   * Set the value of amount
+   *
+   * @return  self
+   */ 
+  public function setAmount($amount)
+  {
+    $this->amount = $amount;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of balance
+   */ 
+  public function getBalance()
+  {
+    return $this->balance;
+  }
+
+  /**
+   * Set the value of balance
+   *
+   * @return  self
+   */ 
+  public function setBalance($balance)
+  {
+    $this->balance = $balance;
 
     return $this;
   }
@@ -70,7 +125,8 @@ class TransactionModel extends CoreModel {
   /**
    * Get the value of type
    */ 
-  public function getType() {
+  public function getType()
+  {
     return $this->type;
   }
 
@@ -79,7 +135,8 @@ class TransactionModel extends CoreModel {
    *
    * @return  self
    */ 
-  public function setType($type) {
+  public function setType($type)
+  {
     $this->type = $type;
 
     return $this;
@@ -88,7 +145,8 @@ class TransactionModel extends CoreModel {
   /**
    * Get the value of created_at
    */ 
-  public function getCreatedAt() {
+  public function getCreatedAt()
+  {
     return $this->created_at;
   }
 
@@ -97,7 +155,8 @@ class TransactionModel extends CoreModel {
    *
    * @return  self
    */ 
-  public function setCreatedAt($created_at) {
+  public function setCreatedAt($created_at)
+  {
     $this->created_at = $created_at;
 
     return $this;
@@ -106,7 +165,8 @@ class TransactionModel extends CoreModel {
   /**
    * Get the value of from_acc
    */ 
-  public function getFromAcc() {
+  public function getFromAcc()
+  {
     return $this->from_acc;
   }
 
@@ -115,7 +175,8 @@ class TransactionModel extends CoreModel {
    *
    * @return  self
    */ 
-  public function setFromAcc($from_acc) {
+  public function setFromAcc($from_acc)
+  {
     $this->from_acc = $from_acc;
 
     return $this;
@@ -124,7 +185,8 @@ class TransactionModel extends CoreModel {
   /**
    * Get the value of to_acc
    */ 
-  public function getToAcc() {
+  public function getToAcc()
+  {
     return $this->to_acc;
   }
 
@@ -133,7 +195,8 @@ class TransactionModel extends CoreModel {
    *
    * @return  self
    */ 
-  public function setToAcc($to_acc) {
+  public function setToAcc($to_acc)
+  {
     $this->to_acc = $to_acc;
 
     return $this;
