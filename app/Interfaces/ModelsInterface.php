80e842f3ad16b918d7d036fdadde3ef48d969638
Danilo Suguimoto <danilo.suguimoto@webjump.com.br>
2021-06-09 15:52:27 -0300

    Changing parameters for the update method

--- app/Interfaces/ModelsInterface.php
+++ app/Interfaces/ModelsInterface.php
@@ -2,16 +2,17 @@
 
 namespace WjCrypto\Interfaces;
 
-interface ModelsInterface {  
+interface ModelsInterface 
+{
   public static function setAttributes(string $tableName, array $columns);
 
   public static function selectDataFrom(string $column, $value);
-  
+
   public static function selectAllData();
 
   public static function insertData(array $data);
 
-  public static function updateData(array $data, string $where, int $id);
+  public static function updateData(array $data, string $whereColumn, $whereValue);
 
   public static function deleteData(string $columns, string $value);
 }
